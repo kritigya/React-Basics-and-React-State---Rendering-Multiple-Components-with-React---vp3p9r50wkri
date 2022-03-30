b00e188b457960faff5c867ddfcaf6d448da7604
kritigya <97790296+kritigya@users.noreply.github.com>
2022-03-30 21:34:49 +0530

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,1 +1,15 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+const App = () => {
+  return (
+    <div id="main">
+    <h1 data-ns-test="project-name">instagram</h1>
+      <div data-ns-test="project-description">hgfdsgf</div></div>
+    </div>
+  )
+}
+
+
+export default App;
 
