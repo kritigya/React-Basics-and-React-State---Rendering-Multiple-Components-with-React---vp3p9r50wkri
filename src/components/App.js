184ebf8636c6ec53a1a612a1a17691e624711c31
kritigya <97790296+kritigya@users.noreply.github.com>
2022-03-30 21:38:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Project from '.components/Project'
 
 const App = () => {
   return (
+    <>
     <div id="main">
     </div>
+    <Project/>
+    <>
   )
 }
 
