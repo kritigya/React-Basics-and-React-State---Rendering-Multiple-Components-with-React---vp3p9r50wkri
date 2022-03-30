c4c640fc6aea6e7aaa8f705e632be7cac4e4733d
kritigya <97790296+kritigya@users.noreply.github.com>
2022-03-30 21:54:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-<h1 data-ns-test="project-name">instagram</h1>
-      <div data-ns-test="project-description">hgfdsgf</div></div>
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
