9c01238a97ccaccff417a2bbe539e82149604799
kritigya <97790296+kritigya@users.noreply.github.com>
2022-03-31 23:52:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,9 @@ import '../styles/App.css';
 const App = () => {
   return (
     <>
-    <div id="main">
     <h1 data-ns-test="project-name>instagram</h1>
     <dl data-ns-test="project-description>my dream project</dl>
-    </div>
+  
     
     <>
   )
