dea617695213f20fe79ef224b6de3c2b5a805c5a
kritigya <97790296+kritigya@users.noreply.github.com>
2022-04-01 20:44:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-  <div id="main">
-      <h1 data-ns-test="project-name">Clone of Ns</h1>
-      <p data-ns-test="project-description">I want to clone Ns app</p>
+    <div id="main">
+      <h1 data-ns-test="Cloning of app">Clone of IndiaMART</h1>
+      <p data-ns-test="app cloning">I want to clone the IndiaMART app</p>
     </div>
   )
 }
-
 
 export default App;
